5499816f7055a6fc18c098ddf7b082a43b9bff9a
Hector <hledezma910@gmail.com>
2024-11-04 11:05:02 -0400

    Create the getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3)
+    if (computerChoice === 0) {
+        return "Rock";
+    } else if (computerChoice === 1){
+        return "Paper";
+    } else {
+        return "Scissors";
+    };
+}
+
+console.log(getComputerChoice())
+
